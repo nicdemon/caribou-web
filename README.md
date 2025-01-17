6ff9ac63b926271d1f1ef557bceee97446e8fdef
Nicolas de Montigny <nicolas.de.montigny0323@gmail.com>
2025-01-17 18:23:26 -0500

    init flet project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# caribou-web
-A Web interface for Caribou
+# Caribou web
+
+This is a web interface for running, monitoring and consulting results for the [Caribou analysis pipeline](https://github.com/bioinfoUQAM/Caribou).
+
+The interface was built using Flet which is the official Flutter library for Python.
+
+Deployment, hosting and computing are done using Firebase.
+
+To deploy the app locally:
+```
+flet run --web
+```
---
+++ src/main.py
@@ -0,0 +1,33 @@
+import flet as ft
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('.secrets/caribou-web-app-firebase-adminsdk-fbsvc-d5c858c0c9.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+def main(page: ft.Page):
+    counter = ft.Text("0", size=50, data=0)
+
+    def increment_click(e):
+        counter.data += 1
+        counter.value = str(counter.data)
+        counter.update()
+
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.ADD, on_click=increment_click
+    )
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                counter,
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+
+
+ft.app(main)
